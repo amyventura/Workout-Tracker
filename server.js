aa75367cd5c6c71294aa2d93a6d2afb37ed3b287
Amy Ventura <amyvntr@yahoo.com>
2020-12-19 09:42:51 -0500

    cleaning up

--- server.js
+++ server.js
@@ -12,18 +12,6 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-// const MongoClient = require('mongodb').MongoClient;
-// const uri = "mongodb+srv://amyvntr:Irocks17@cluster0.gyzzr.mongodb.net/workout?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, {
-//     useNewUrlParser: true
-// });
-// client.connect(err => {
-//     const collection = client.db("test").collection("devices");
-//     // perform actions on the collection object
-//     client.close();
-// });
-
-
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/ancient-fortress-62454", {
     useNewUrlParser: true,
     useFindAndModify: false,
